49833ef8ad53415699b07017390a6584289a2731
Dan Shalev <shalev444@gmail.com>
2020-11-06 03:17:36 +0200

    UsersGroup Table: complex key with forigen keys added

--- src/main/java/com/tie/model/dao/GroupsUsers.java
+++ src/main/java/com/tie/model/dao/GroupsUsers.java
@@ -1,26 +1,64 @@
 package com.tie.model.dao;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.JoinColumn;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@Data
+class GroupsUsersKey implements Serializable
+{
+    @Column(name = "phone_number")
+    String phoneNumber;
+
+    @Column(name = "group_id")
+    String groupId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof GroupsUsersKey)) {
+            return false;
+        }
+
+        GroupsUsersKey other = (GroupsUsersKey)o;
+        return Objects.equals(this.phoneNumber, other.phoneNumber) && Objects.equals(this.groupId, other.groupId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber, groupId);
+    }
+}
 
 
 @Entity
-@Getter
-@Setter
-@ToString
-@Table(name = "GroupsUsers")
-public class GroupsUsers {
+@Data
+@Table(name = "groups_users")
+public class GroupsUsers
+{
+    @EmbeddedId
+    GroupsUsersKey id;
 
-    @Id
-    @Column(name = "user_id")
-    private String userId;
+    @ManyToOne
+    @MapsId("group_id")
+    @JoinColumn(name = "group_id")
+    Group group;
 
-    @Column(name = "group_id")
-    private String groupId;
+    @ManyToOne
+    @MapsId("phone_number")
+    @JoinColumn(name = "phone_number")
+    User user;
 }
