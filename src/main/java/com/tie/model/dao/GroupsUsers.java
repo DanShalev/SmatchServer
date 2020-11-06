21faf631694b064b0d6e88671079a74ba8988ea0
Dan Shalev <shalev444@gmail.com>
2020-11-06 12:50:16 +0200

    UserGroups Table: repository added

--- src/main/java/com/tie/model/dao/GroupsUsers.java
+++ src/main/java/com/tie/model/dao/GroupsUsers.java
@@ -12,35 +12,16 @@ import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.JoinColumn;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
 @Data
 class GroupsUsersKey implements Serializable
 {
     @Column(name = "phone_number")
-    String phoneNumber;
+    private String phoneNumber;
 
     @Column(name = "group_id")
-    String groupId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof GroupsUsersKey)) {
-            return false;
-        }
-
-        GroupsUsersKey other = (GroupsUsersKey)o;
-        return Objects.equals(this.phoneNumber, other.phoneNumber) && Objects.equals(this.groupId, other.groupId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(phoneNumber, groupId);
-    }
+    private String groupId;
 }
 
 
@@ -50,15 +31,15 @@ class GroupsUsersKey implements Serializable
 public class GroupsUsers
 {
     @EmbeddedId
-    GroupsUsersKey id;
+    private GroupsUsersKey id;
 
     @ManyToOne
     @MapsId("group_id")
     @JoinColumn(name = "group_id")
-    Group group;
+    private Group group;
 
     @ManyToOne
     @MapsId("phone_number")
     @JoinColumn(name = "phone_number")
-    User user;
+    private User user;
 }
---
+++ src/main/java/com/tie/repository/GroupsUsersRepository.java
@@ -0,0 +1,7 @@
+package com.tie.repository;
+
+
+import com.tie.model.dao.GroupsUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupsUsersRepository extends JpaRepository<GroupsUsers, Integer> { }
