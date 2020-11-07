c3cf39dbd2b3da9401b874a1ab7243004449ef69
guy <guymoshe5@gmail.com>
2020-11-07 23:08:04 +0200

    add + edit group APIs

--- src/main/java/com/tie/controller/GroupController.java
+++ src/main/java/com/tie/controller/GroupController.java
@@ -1,9 +1,11 @@
 package com.tie.controller;
 
+import com.tie.model.dao.Group;
 import com.tie.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/groups")
@@ -12,5 +14,18 @@ public class GroupController {
 
     private final GroupService groupService;
 
+    @PostMapping("/addGroup")
+    public Group addUser(@RequestBody Group group) {
+        return groupService.addGroup(group);
+    }
 
+    @PutMapping("/editGroup")
+    public Group editUser(@RequestBody Group group) {
+        return groupService.editGroup(group);
+    }
+
+    @GetMapping("/getGroups")
+    public List<Group> getUsers() {
+        return groupService.getGroups();
+    }
 }
--- src/main/java/com/tie/service/GroupService.java
+++ src/main/java/com/tie/service/GroupService.java
@@ -1,9 +1,13 @@
 package com.tie.service;
 
+import com.tie.model.dao.Group;
 import com.tie.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +16,18 @@ public class GroupService {
 
     private final GroupRepository groupRepository;
 
+    public Group addGroup(Group group) {
+        group.setId(UUID.randomUUID().toString());
+        groupRepository.save(group);
+        return group;
+    }
+
+    public Group editGroup(Group group) {
+        groupRepository.save(group);
+        return group;
+    }
+
+    public List<Group> getGroups() {
+        return groupRepository.findAll();
+    }
 }
