468575fa54021ada6c93827bfa4452f0baccb200
Elad Ben David <elad.bendavid2@gmail.com>
2020-11-18 08:29:56 +0000

    refactor to make cleaner code

--- src/main/java/com/tie/controller/GroupController.java
+++ src/main/java/com/tie/controller/GroupController.java
@@ -1,14 +1,10 @@
 package com.tie.controller;
 
 import com.tie.model.dao.Group;
-import com.tie.model.dao.SubscriptionId;
-import com.tie.model.dao.User;
 import com.tie.service.GroupService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,39 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/groups")
+@RequestMapping("/group")
 @RequiredArgsConstructor
 public class GroupController {
 
     private final GroupService groupService;
 
-    @PostMapping("/")
-    public Group addGroup(@RequestBody Group group) {
-        return groupService.addGroup(group);
-    }
-
-    @PutMapping("/")
-    public Group editGroup(@RequestBody Group group) {
-        return groupService.editGroup(group);
-    }
-
-    @GetMapping("/")
+    @GetMapping("/get")
     public List<Group> getGroups() {
         return groupService.getGroups();
     }
 
-    @DeleteMapping("/")
-    public void leaveGroup(@RequestBody SubscriptionId subscriptionId) {
-        groupService.leaveGroup(subscriptionId);
+    @PostMapping("/add")
+    public Group addGroup(@RequestBody Group group) {
+        return groupService.addGroup(group);
     }
 
-    @GetMapping("/{groupId}/users")
-    public List<User> getGroupUsers(@PathVariable String groupId) {
-        return groupService.getGroupUsers(groupId);
+    @PutMapping("/edit")
+    public Group editGroup(@RequestBody Group group) {
+        return groupService.editGroup(group);
     }
 
-    @PostMapping("/{groupId}/{userId}/addUser")
-    public SubscriptionId addUserToGroup(@PathVariable String groupId, @PathVariable String userId) {
-        return groupService.addUserToGroup(groupId, userId);
-    }
+
 }
--- src/main/java/com/tie/service/GroupService.java
+++ src/main/java/com/tie/service/GroupService.java
@@ -1,14 +1,11 @@
 package com.tie.service;
 
 import com.tie.model.dao.Group;
-import com.tie.model.dao.Subscription;
-import com.tie.model.dao.SubscriptionId;
-import com.tie.model.dao.User;
 import com.tie.repository.GroupRepository;
-import com.tie.repository.SubscriptionRepository;
+
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,8 +18,6 @@ import org.springframework.web.server.ResponseStatusException;
 public class GroupService {
 
     private final GroupRepository groupRepository;
-    private final SubscriptionRepository subscriptionRepository;
-    private final UserService userService;
 
     public Group addGroup(Group group) {
         group.setId(UUID.randomUUID().toString());
@@ -40,49 +35,9 @@ public class GroupService {
         return groupRepository.findAll();
     }
 
-    public void leaveGroup(SubscriptionId subscriptionId) {
-        Subscription subscription = getSubscriptionById(subscriptionId);
-        subscriptionRepository.delete(subscription);
-    }
-
-    public List<User> getGroupUsers(String groupId) {
-        List<Subscription> groupSubscriptions = subscriptionRepository.findSubscriptionsByGroupId(groupId);
-        if (groupSubscriptions.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    String.format("Group Id %s doesn't exists or is empty", groupId));
-        }
-        return groupSubscriptions.stream().map(Subscription::getUser).collect(Collectors.toList());
-    }
-
-    private Subscription getSubscriptionById(SubscriptionId subscriptionId) {
-        return subscriptionRepository.findSubscriptionById(subscriptionId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        String.format("%s is not subscribed to group %s", subscriptionId.getUserId(),
-                                subscriptionId.getGroupId())));
-    }
-
-    private void verifySubscriptionDoesntExist(SubscriptionId subscriptionId) {
-        subscriptionRepository.findSubscriptionById(subscriptionId).ifPresent(
-                sub -> { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("User %s is already subscribed to group %s.",
-                                                                                                 subscriptionId.getUserId(), subscriptionId.getGroupId())); }
-        );
-    }
-
-    private Group verifyGroupExists(String groupId) {
+    Group verifyGroupExists(String groupId) {
         return groupRepository.findGroupById(groupId).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
                         String.format("Group %s doesn't exist.", groupId)));
-    }
-
-    public SubscriptionId addUserToGroup(String groupId, String userId) {
-        User user = userService.verifyUserExists(userId);
-        Group group = verifyGroupExists(groupId);
-
-        SubscriptionId subscriptionId = new SubscriptionId(userId, groupId);
-        verifySubscriptionDoesntExist(subscriptionId);
-
-        Subscription subs = new Subscription(subscriptionId, group, user);
-        subscriptionRepository.save(subs);
-        return subscriptionId;
     }
 }
