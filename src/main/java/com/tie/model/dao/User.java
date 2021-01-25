b6dcfddbd5b63ab37f5ad94882936a25a9161ff0
Dan Shalev <shalev444@gmail.com>
2021-01-25 10:12:19 +0000

    Adding FE server swiping page API connection, adding DB dump file

--- src/main/java/com/tie/model/dao/User.java
+++ src/main/java/com/tie/model/dao/User.java
@@ -26,6 +26,15 @@ public class User {
     @Column(name = "age")
     private int age;
 
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "image_url2")
+    private String imageUrl2;
+
+    @Column(name = "image_url3")
+    private String imageUrl3;
+
     @JsonIgnore
     @ToString.Exclude
     @OneToMany(mappedBy = "user")
