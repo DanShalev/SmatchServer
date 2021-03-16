12ad09859f9fffb4b7fe3e7a1e0fd463ff2904cd
Elad Ben David <elad.bendavid2@gmail.com>
2021-03-16 19:19:38 +0000

    naming convention applied

---
+++ src/main/java/com/tie/controller/MatchController.java
@@ -0,0 +1,28 @@
+package com.tie.controller;
+
+import com.tie.service.MatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/match")
+@RequiredArgsConstructor
+public class MatchController {
+
+    private final MatchService matchService;
+
+    @PostMapping("/like/{groupId}/{likeUserId}/{otherUserId}")
+    public boolean insertLike(@PathVariable String groupId, @PathVariable String likeUserId, @PathVariable String otherUserId) {
+        return matchService.insertLike(groupId, likeUserId, otherUserId);
+    }
+
+    @PostMapping("/dislike/{groupId}/{dislikeUserId}/{otherUserId}")
+    public void insertDislike(@PathVariable String groupId, @PathVariable String dislikeUserId, @PathVariable String otherUserId) {
+        matchService.insertDislike(groupId, dislikeUserId, otherUserId);
+    }
+
+}
